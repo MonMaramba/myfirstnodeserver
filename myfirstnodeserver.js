c7a6d8e9b9c6c1c8f23fd7c8bdbdb35de1b12a47
unknown <monmaramba@yahoo.com>
2018-08-07 19:39:47 -0700

    this works

---
+++ myfirstnodeserver.js
@@ -0,0 +1,46 @@
+// Require/import the HTTP module
+var http = require("http");
+
+// Define a port to listen for incoming requests
+var PORT = 7000;
+var PORT2 = 7500;
+
+// Create a generic function to handle requests and responses
+function handleRequest(request, response) {
+
+  // Send the below string to the client when the user visits the PORT URL
+  response.end("You win some ice cream from Baskin and Robbins" + request.url);
+}
+
+function handleRequest2(request, response) {
+
+    // Send the below string to the client when the user visits the PORT URL
+    response.end("Get lost....  find yourself another server" + request.url);
+  }
+
+// Use the Node HTTP package to create our server.
+// Pass the handleRequest function to empower it with functionality.
+var server = http.createServer(handleRequest);
+
+// Start our server so that it can begin listening to client requests.
+server.listen(PORT, function() {
+
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
+
+var server2 = http.createServer(handleRequest2);
+
+// Start our server so that it can begin listening to client requests.
+server.listen(PORT, function() {
+
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
+
+
+server2.listen(PORT2, function() {
+
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT2);
+  });
